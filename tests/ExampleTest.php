f5f90b32b0821ae8450952aa04ff73a0597ae7a7
anproagile <anproagile@gmail.com>
2020-09-29 10:16:49 -0400

    Update index.blade.php

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -3,12 +3,18 @@
 namespace Larsklopstra\Nebula\Tests;
 
 use Larsklopstra\Nebula\NebulaServiceProvider;
-use Orchestra\Testbench\TestCase as BaseTestCase;
+use Orchestra\Testbench\TestCase;
 
-class TestCase extends BaseTestCase
+class ExampleTest extends TestCase
 {
     protected function getPackageProviders($app)
     {
         return [NebulaServiceProvider::class];
+    }
+
+    /** @test */
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
     }
 }
